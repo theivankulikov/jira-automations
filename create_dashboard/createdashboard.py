cc9bb1cc35c7d1f3424760e2d87d809ccb91f96e
Ivan Kulikov <ivankulich@gmail.com>
2023-12-29 17:48:02 +0200

    add setting for jira url and created template for createdashboard function

---
+++ create_dashboard/createdashboard.py
@@ -0,0 +1,52 @@
+# https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-dashboards/
+
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+import authdata
+import settings
+
+
+def createdashboard(db_name):
+    created_dashboard = "Dasboard is not created"
+
+
+# Authentification
+    auth = HTTPBasicAuth(authdata.email, authdata.api_token)
+
+
+# Searching dashboard with name settings.dashboard_name
+    
+    print("Searching dashboard.")
+
+    headers = {
+        "Accept": "application/json",
+    }
+
+    params = {
+        'filterName' : db_name
+    }
+
+    response = requests.get(
+        settings.jira_url + "/rest/api/3/dashboard/search",
+        params=params,
+        headers=headers,
+        auth=auth
+    )
+
+    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+
+
+# Clearing the dashboard
+    if len(json.loads(response.text)['values']) >0:
+        print("Dashboard have been found. Clearing dashboard.")
+
+        print("Dashboard is cleared.")
+    else:
+        print("Dashboard not found. Creation of newone is started.")
+        
+        print("Dashboard is created.")
+
+
+    return created_dashboard
--- create_dashboard/settings.py
+++ create_dashboard/settings.py
@@ -1,2 +1,3 @@
+jira_url = "url to jira"
 dashboard_name = "dashboard name"
 label_name = "label for filtering items"
